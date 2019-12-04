dba90b031c0ef7b3af94f630bbbd9c92f4c26522
Eddie O'Neill <ejosephoneill@gmail.com>
2019-12-04 17:40:55 -0500

    updated the solution for isSameStr problem

--- isSameStr.playground/Contents.swift
+++ isSameStr.playground/Contents.swift
@@ -1,36 +1,63 @@
 import UIKit
 
-func isSameStr(arr: [String]) -> Bool {
-    var firstCheck = ""
-    var passedLetters = ""
-    var previousWord = ""
-    var count = 0
-    
-    for str in arr {
-        for char in str {
+var passedWord = ""
+var previousLetter = ""
+var found$ = false
+var count = 1
+
+func stringFilter(str: String) {
+    for char in str { // n
+        if found$ {
+            passedWord += String(char)
             count += 1
-            if count == str.count && char != "$" {
-                passedLetters += "\(firstCheck)\(char)"
-                firstCheck = ""
-            } else if char != "$" {
-                passedLetters += firstCheck
-                firstCheck = String(char)
-            } else {
-                firstCheck = ""
-            }
-        }
-        if previousWord == "" {
-            previousWord = passedLetters
-            passedLetters = ""
-        } else if passedLetters != previousWord {
-            return false
+        } else if char == "$" {
+            found$ = true
+            count += 1
+        } else if count == str.count {
+            passedWord += "\(previousLetter)\(char)"
         } else {
-            previousWord = passedLetters
-            passedLetters = ""
+            passedWord += previousLetter
+            previousLetter = String(char)
+            count += 1
         }
-        count = 0
     }
-    return true
+}
+
+func reset() {
+    previousLetter = ""
+    passedWord = ""
+    count = 1
+}
+
+func isSameStr(arr: [String]) -> Bool {
+    guard !arr.isEmpty else { return false }
+    guard arr.count > 1 else { return true }
+    
+    var strArr = arr
+    
+    stringFilter(str: strArr.last!) // n
+    strArr[strArr.count - 1] = passedWord
+    reset()
+    
+    if found$ {
+        found$ = false
+        return isSameStr(arr: strArr)
+    }
+    
+    stringFilter(str: strArr[arr.count - 2])
+    strArr[strArr.count - 2] = passedWord
+    reset()
+    
+    if found$ {
+        found$ = false
+        return isSameStr(arr: strArr)
+    } else if strArr.last! != strArr[strArr.count - 2] {
+        return false
+    } else {
+        strArr.popLast()
+        return isSameStr(arr: strArr)
+    }
+    
 }
 
 isSameStr(arr: ["af$stt$", "ast"])
